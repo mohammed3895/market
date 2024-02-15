6c32e5623cb1336a8cdeb4841b5aad294b134c01
mohammed sameer <85784627+mohammed3895@users.noreply.github.com>
2024-02-15 21:44:21 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config = {
     },
     extend: {
       dropShadow: {
-        "3xl": "0 35px 35px rgba(255, 255, 255, 0.25)",
+        "3xl": "0 35px 35px rgba(255, 255, 255, 0.2)",
         "4xl": [
           "0 35px 35px rgba(0, 0, 0, 0.25)",
           "0 45px 65px rgba(0, 0, 0, 0.15)",
